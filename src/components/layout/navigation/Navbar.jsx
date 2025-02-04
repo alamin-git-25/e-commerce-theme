e0e5371877e4bc093f5fc31006994b7c70f75f7a
Alamin-dev-sys <alamin.dev.sys@gmail.com>
2025-02-04 13:57:48 +0600

    first commit

---
+++ src/components/layout/navigation/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ChevronDown, Search, User, Heart, ShoppingCart, Phone } from "lucide-react";
+
+const Navbar = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default Navbar;
+
